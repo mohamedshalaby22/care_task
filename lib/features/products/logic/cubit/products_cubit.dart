d07998cb4caec8344481091d86d436e8dddd8295
mohamedshalaby <shalabymohamed578@mail.com>
2025-02-07 17:34:00 +0200

    save

--- lib/features/products/logic/cubit/products_cubit.dart
+++ lib/features/products/logic/cubit/products_cubit.dart
@@ -1,6 +1,10 @@
 import 'package:care_task/features/products/data/repo/products_repo.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import '../../../../core/widgets/app_snack_bar.dart';
+import '../../../favourite/data/models/product_model.dart';
 import '../../data/models/product_list_response.dart';
 part 'products_state.dart';
 part 'products_cubit.freezed.dart';
@@ -16,6 +20,40 @@ class ProductsCubit extends Cubit<ProductsState> {
       emit(ProductsState.success(products));
     } catch (e) {
       emit(ProductsState.error(message: e.toString()));
+    }
+  }
+
+  ProductModel convertToProductModel(ProductListResponse product) {
+    return ProductModel(
+      id: product.id,
+      title: product.title,
+      price: product.price.toString(),
+      image: product.image,
+    );
+  }
+
+  Future<void> toggleFavorite(BuildContext context, Box<ProductModel> box,
+      ProductListResponse product) async {
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+
+    final productModel = convertToProductModel(product);
+    if (box.containsKey(product.id)) {
+      await box.delete(product.id);
+      if (context.mounted) {
+        AppSnackBar.showSnackBarWidget(
+          context: context,
+          message: 'Product removed from favourite!',
+          backColor: Colors.red[300]!,
+        );
+      }
+    } else {
+      await box.put(product.id, productModel);
+      if (context.mounted) {
+        AppSnackBar.showSnackBarWidget(
+          context: context,
+          message: 'Product added to favourite!',
+        );
+      }
     }
   }
 }
--- lib/features/products/ui/product_list_view.dart
+++ lib/features/products/ui/product_list_view.dart
@@ -1,10 +1,11 @@
 import 'package:care_task/core/routing/routes.dart';
-import 'package:care_task/core/widgets/app_snack_bar.dart';
 import 'package:care_task/features/products/data/models/product_list_response.dart';
 import 'package:care_task/features/products/ui/product_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import '../../favourite/data/models/product_model.dart';
+import '../logic/cubit/products_cubit.dart';
 
 class ProductListView extends StatelessWidget {
   const ProductListView({super.key, required this.products});
@@ -13,8 +14,7 @@ class ProductListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder(
-      valueListenable:
-          Hive.box<ProductModel>('favourite').listenable(), 
+      valueListenable: Hive.box<ProductModel>('favourite').listenable(),
       builder: (context, box, child) {
         return Expanded(
           child: ListView.builder(
@@ -29,7 +29,9 @@ class ProductListView extends StatelessWidget {
                   title: product.title,
                   price: product.price.toString(),
                   isFavourite: isFavorite,
-                  onPress: () => _toggleFavorite(context, box, product),
+                  onPress: () => context
+                      .read<ProductsCubit>()
+                      .toggleFavorite(context, box, product),
                 ),
               );
             },
@@ -41,43 +43,9 @@ class ProductListView extends StatelessWidget {
 
   void _navigateToProductDetails(
       BuildContext context, ProductListResponse product) {
-    final productModel = _convertToProductModel(product);
+    final productModel =
+        context.read<ProductsCubit>().convertToProductModel(product);
     Navigator.pushNamed(context, Routes.productDetailsScreen,
         arguments: productModel);
-  }
-
-  ProductModel _convertToProductModel(ProductListResponse product) {
-    return ProductModel(
-      id: product.id,
-      title: product.title,
-      price: product.price.toString(),
-      image: product.image,
-    );
-  }
-
-  Future<void> _toggleFavorite(BuildContext context, Box<ProductModel> box,
-      ProductListResponse product) async {
-    ScaffoldMessenger.of(context).hideCurrentSnackBar();
-
-    final productModel = _convertToProductModel(product);
-
-    if (box.containsKey(product.id)) {
-      await box.delete(product.id);
-      if (context.mounted) {
-        AppSnackBar.showSnackBarWidget(
-          context: context,
-          message: 'Product removed from favourite!',
-          backColor: Colors.red[300]!,
-        );
-      }
-    } else {
-      await box.put(product.id, productModel);
-      if (context.mounted) {
-        AppSnackBar.showSnackBarWidget(
-          context: context,
-          message: 'Product added to favourite!',
-        );
-      }
-    }
   }
 }
